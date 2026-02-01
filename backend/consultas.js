b02c0ce65e57642b61a39456e0269145321c3ef8
Nicolás Rivero <nicog.rivero@gmail.com>
2026-02-01 20:12:00 -0300

    Prepara conexion DB para deploy

--- backend/consultas.js
+++ backend/consultas.js
@@ -2,12 +2,14 @@ const { Pool } = require('pg');
 require('dotenv').config();
 
 const pool = new Pool({
+    connectionString: process.env.DATABASE_URL,
     host: process.env.DB_HOST,
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
     database: process.env.DB_NAME,
     port: process.env.DB_PORT,
-    allowExitOnIdle: true
+    allowExitOnIdle: true,
+    ssl: process.env.DATABASE_URL ? { rejectUnauthorized: false } : false
 });
 
 const obtenerProductos = async () => {
